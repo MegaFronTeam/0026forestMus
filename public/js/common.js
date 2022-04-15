943d4df5a34ee3f08178719b89ec3a891d3e63b6
Pavel <wol1414@gmail.com>
2022-04-15 10:47:10 +0300

    edit

--- public/js/common.js
+++ public/js/common.js
@@ -160,47 +160,7 @@ const JSCCommon = {
 			document.body.insertAdjacentHTML("beforeend", '<div class="browsehappy">	<p class=" container">К сожалению, вы используете устаревший браузер. Пожалуйста, <a href="http://browsehappy.com/" target="_blank">обновите ваш браузер</a>, чтобы улучшить производительность, качество отображаемого материала и повысить безопасность.</p></div>');
 		}
 	},
-	sendForm() {
-		var gets = (function () {
-			var a = window.location.search;
-			var b = new Object();
-			var c;
-			a = a.substring(1).split("&");
-			for (var i = 0; i < a.length; i++) {
-				c = a[i].split("=");
-				b[c[0]] = c[1];
-			}
-			return b;
-		})();
-		// form
-		$(document).on('submit', "form", function (e) {
-			e.preventDefault();
-			const th = $(this);
-			var data = th.serialize();
-			th.find('.utm_source').val(decodeURIComponent(gets['utm_source'] || ''));
-			th.find('.utm_term').val(decodeURIComponent(gets['utm_term'] || ''));
-			th.find('.utm_medium').val(decodeURIComponent(gets['utm_medium'] || ''));
-			th.find('.utm_campaign').val(decodeURIComponent(gets['utm_campaign'] || ''));
-			$.ajax({
-				url: 'action.php',
-				type: 'POST',
-				data: data,
-			}).done(function (data) {
-
-				Fancybox.close();
-				Fancybox.show([{ src: "#modal-thanks", type: "inline" }]);
-				// window.location.replace("/thanks.html");
-				setTimeout(function () {
-					// Done Functions
-					th.trigger("reset");
-					// $.magnificPopup.close();
-					// ym(53383120, 'reachGoal', 'zakaz');
-					// yaCounter55828534.reachGoal('zakaz');
-				}, 4000);
-			}).fail(function () { });
-
-		});
-	},
+ 
 	heightwindow() {
 		// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 		let vh = window.innerHeight * 0.01;
@@ -222,7 +182,7 @@ const JSCCommon = {
 			}
 			else {
 				let destination = $(elementClick).offset().top;
-				$('html, body').animate({ scrollTop: destination - 80 }, 0);
+				$('html, body').animate({ scrollTop: destination - 80 }, 1000);
 				return false;
 			}
 		});
@@ -252,35 +212,14 @@ const JSCCommon = {
 			};
 		}, { passive: true });
 	},
-	makeDDGroup() {
-		let parents = document.querySelectorAll('.dd-group-js');
-		for (let parent of parents) {
-			if (parent) {
-				// childHeads, kind of funny))
-				let ChildHeads = parent.querySelectorAll('.dd-head-js:not(.disabled)');
-				$(ChildHeads).click(function () {
-					let clickedHead = this;
-
-					$(ChildHeads).each(function () {
-						if (this === clickedHead) {
-							//parent element gain toggle class, style head change via parent
-							$(this.parentElement).toggleClass('active');
-							$(this.parentElement).find('.dd-content-js').slideToggle(function () {
-								$(this).toggleClass('active');
-							});
-						}
-						else {
-							$(this.parentElement).removeClass('active');
-							$(this.parentElement).find('.dd-content-js').slideUp(function () {
-								$(this).removeClass('active');
-							});
-						}
-					});
-
-				});
-			}
-		}
-	},
+	makeDDGroup() { 
+		// let ChildHeads = parent.querySelectorAll('.dd-head-js:not(.disabled)');
+		$('.dd-head-js:not(.disabled)').click(function () {
+			$(this.parentElement).find('.dd-content-js').slideToggle(function () {
+				$(this).toggleClass('active');
+			}); 
+		}) 
+		} 
 };
 const $ = jQuery;
 
@@ -294,7 +233,7 @@ function eventHandler() {
 	JSCCommon.heightwindow();
 	JSCCommon.makeDDGroup();
 	// JSCCommon.toggleShow(".catalog-block__toggle--desctop", '.catalog-block__dropdown');
-	// JSCCommon.animateScroll();
+	JSCCommon.animateScroll();
 	
 	// JSCCommon.CustomInputFile(); 
 	var x = window.location.host;
@@ -398,7 +337,7 @@ function eventHandler() {
 
 		//let targetDate = new Date(now.getFullYear(), now.getMonth(), d, h, m, s);
 		//force date
-		let targetDate = new Date(2022, 5,1);
+		let targetDate = new Date(now.getFullYear(), now.getMonth(),now.getDate(), now.getHours() + 14 );
 
 
 		//interval
@@ -410,6 +349,7 @@ function eventHandler() {
 
 	function tikTakReadOut(parent,targetDate, ReadOutID, days, hours, minutes, seconds){
 		let now = new Date();
+		// let timeLeft = (targetDate - now) / 1000;
 		let timeLeft = (targetDate - now) / 1000;
 
 		if (timeLeft < 1) {
